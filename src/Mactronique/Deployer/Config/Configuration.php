564f539e98ed30a00bf16a2e333e2893ea25542d
macintoshplus <jean-baptiste@nahan.fr>
2015-08-07 21:13:55 +0200

    add configuration checker

---
+++ src/Mactronique/Deployer/Config/Configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Mactronique\Deployer\Config;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('deployer');
+
+        $rootNode
+                ->children()
+                    ->booleanNode('enable-email')
+                        ->defaultFalse()
+                    ->end()
+                    ->arrayNode('projects')
+                        ->useAttributeAsKey('name')
+                        ->prototype('array')
+                            ->children()
+                                ->scalarNode('url')->isRequired()->end()
+                                ->booleanNode('enable')
+                                    ->defaultTrue()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end();
+
+        return $treeBuilder;
+    }
+}
