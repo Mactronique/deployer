c0afd6797886e5d10acd151350a4a6575554c69d
macintoshplus <jean-baptiste@nahan.fr>
2015-08-07 21:10:52 +0200

    add service for project

---
+++ src/Mactronique/Deployer/Manager/ProjectManager.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Mactronique\Deployer\Manager;
+
+class ProjectManager
+{
+    private $projects;
+
+    public function __construct(array $projects)
+    {
+        $this->$projects = $projects;
+    }
+
+    public function add($name, $url)
+    {
+
+    }
+
+    public function getList()
+    {
+
+    }
+
+    public function remove($name)
+    {
+
+    }
+
+    private function save()
+    {
+
+    }
+}
