7fad28193986fc06704087ad7663b935a73ac9f4
MacintoshPlus <jean-baptiste@nahan.fr>
2015-08-10 21:13:05 +0200

    add register CVS Adapter

--- src/Mactronique/Deployer/Manager/ProjectManager.php
+++ src/Mactronique/Deployer/Manager/ProjectManager.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace Mactronique\Deployer\Manager;
+use Mactronique\Deployer\Adapter\CvsAdapter;
 
 class ProjectManager
 {
     private $projects;
+
+    /**
+     * Array contains all system of Control of Version for Sources
+     */
+    private $system;
 
     public function __construct(array $projects)
     {
@@ -24,6 +30,11 @@ class ProjectManager
     public function remove($name)
     {
 
+    }
+
+    public function addCVS(CvsAdapter $system)
+    {
+        $this->system[$system->getName()] = $system;
     }
 
     private function save()
