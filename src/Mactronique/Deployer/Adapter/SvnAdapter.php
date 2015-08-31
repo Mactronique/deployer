67e99fca0de1334ba04e35e7eba3749fbabe6e5c
macintoshplus <jean-baptiste@nahan.fr>
2015-08-31 07:49:53 +0200

    add svn adapter

---
+++ src/Mactronique/Deployer/Adapter/SvnAdapter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mactronique\Deployer\Adapter;
+
+class SvnAdapter implements CvsAdapter
+{
+    public function extractTo($url, $path, $version = null)
+    {
+        //1 open process
+        //2 get tool path
+        //3 launch process
+    }
+}
